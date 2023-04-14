ca6db2e71ef344b56d5b285d4c6b410f18b18ce5
lucashasperue <lucashasperue@gmail.com>
2023-04-14 20:22:46 -0300

    primerProyectoJS

---
+++ js/01.js
@@ -0,0 +1,25 @@
+// Variables 
+
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor 
+
+var disponible;  // Iniciamos la variable pero sin valor
+
+// producto = true; // Resignando el valor de la variable
+
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible = true,
+    categoria = 'Computadoras';
+    
+
+// var 1disponible
+var disponible_;
+
+// Estilos para las variables
+var nombre_producto = 'Monitor HD'; // Underscore
+var nombreProducto = 'Monitor HD'; // Camelcase
+var NombreProducto = 'Monitor HD'; // Pascal case
+var nombreproducto = 'Monitor HD';
+
+console.log(producto);
